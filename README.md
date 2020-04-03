b8c36e44a3ba06f5129af5d69d4d65150df100b8
Calvin Cheng <linchuan.cheng@gmail.com>
2020-04-03 19:57:59 +0800

    Add tests
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/3929911652ee2902830b/maintainability)](https://codeclimate.com/github/calvinchengx/goshopify/maintainability)
+
 # golang SDK for Shopify Admin APIs
+
+A golang library that interacts with Shopify Admin REST APIs.
+
+## Tests and coverage
+
+```bash
+go test -coverprofile c.out ./...
+go tool cover -html=c.out
+
+# or simply
+./test.sh
+```
--- client/client_test.go
+++ client/client_test.go
@@ -33,6 +33,7 @@ func TestShopifyClientPost(t *testing.T) {
 
 	s := client.New("key", "secret", "test")
 
+	// test data
 	c := &customer.Customer{
 		Email:     "testemail@example.com",
 		FirstName: "John",
@@ -40,6 +41,7 @@ func TestShopifyClientPost(t *testing.T) {
 	}
 	payload := &customer.Payload{Customer: c}
 
+	// test data
 	jsonData := `{"id":"122345","first_name":"John","last_name":"Freud","email":"testemail@example.com"}`
 	r := ioutil.NopCloser(bytes.NewReader([]byte(jsonData)))
 	mockHTTPClient := &mock.HTTPClient{}
@@ -55,6 +57,33 @@ func TestShopifyClientPost(t *testing.T) {
 		log.Fatal(err)
 	}
 	result, err := s.Client.Post("/customers.json", b)
+
+	// assertion
 	assert.Equal("John", result["first_name"])
+	assert.Nil(err)
+}
+
+func TestShopifyClientGet(t *testing.T) {
+	assert := assert.New(t)
+
+	s := client.New("key", "secret", "test")
+
+	// test data
+	jsonData := `{"customers": [ {"id":"122345","first_name":"John","last_name":"Freud","email":"testemail@example.com"} ] }`
+	r := ioutil.NopCloser(bytes.NewReader([]byte(jsonData)))
+	mockHTTPClient := &mock.HTTPClient{}
+	mockHTTPClient.DoFn = func(*http.Request) (*http.Response, error) {
+		return &http.Response{
+			Body: r,
+		}, nil
+	}
+	s.Client.HTTPClient = mockHTTPClient
+
+	result, err := s.Client.Get("/customers.json")
+
+	// assertion
+	resultList := result["customers"].([]interface{})
+	result0 := resultList[0].(map[string]interface{})
+	assert.Equal("John", result0["first_name"].(string))
 	assert.Nil(err)
 }

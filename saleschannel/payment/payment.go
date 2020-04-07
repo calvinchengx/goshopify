aa79b5fa25c0d29fc1408d07e5671e4601768407
Calvin Cheng <linchuan.cheng@gmail.com>
2020-04-07 11:53:23 +0800

    Implement CreatePayment, which uses the result from Store CreditCard
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- saleschannel/payment/payment.go
+++ saleschannel/payment/payment.go
@@ -2,6 +2,7 @@ package payment
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/calvinchengx/goshopify/client"
 )
@@ -16,11 +17,6 @@ type Service struct {
 	Shopify *client.Shopify
 }
 
-// Payload encapsulates our credit card object
-type Payload struct {
-	CreditCard *CreditCard `json:"credit_card"`
-}
-
 // CreditCard models the credit card object
 type CreditCard struct {
 	Number            string `json:"number"`
@@ -31,9 +27,36 @@ type CreditCard struct {
 	VerificationValue string `json:"verififcation_value"`
 }
 
-// Store saves Credit Card to vault
-func (s *Service) Store(CreditCard *CreditCard) (map[string]interface{}, error) {
-	payload := &Payload{CreditCard}
+// Payment models the payment details
+type Payment struct {
+	RequestDetails *RequestDetails `json:"request_details"`
+	Amount         string          `json:"amount"`
+	SessionID      string          `json:"session_id"`
+	UniqueToken    string          `json:"unique_token"`
+}
+
+// RequestDetails models the http request details in the payment object
+type RequestDetails struct {
+	IPAddress      string `json:"ip_address"`
+	AcceptLanguage string `json:"accept_language"`
+	UserAgent      string `json:"user_agent"`
+}
+
+// Store saves Credit Card to vault and returns a session ID
+func (s *Service) Store(cc *CreditCard) (map[string]interface{}, error) {
+	payload := &map[string]*CreditCard{
+		"credit_card": cc,
+	}
 	b, _ := json.Marshal(payload)
 	return s.Shopify.Client.PostToVault(b)
+}
+
+// CreatePayment creates a payment on a checkout using the session ID returned by the card vault
+func (s *Service) CreatePayment(token string, p *Payment) (map[string]interface{}, error) {
+	payload := &map[string]*Payment{
+		"payment": p,
+	}
+	b, _ := json.Marshal(payload)
+	resource := fmt.Sprintf("/checkouts/%s/payments.json", token)
+	return s.Shopify.Client.Post(resource, b)
 }
--- saleschannel/payment/payment_test.go
+++ saleschannel/payment/payment_test.go
@@ -51,3 +51,40 @@ func TestShopifyStoreCreditCard(t *testing.T) {
 	assert.Equal("83hru3obno3hu434b3u", result["id"])
 	assert.Nil(err)
 }
+
+func TestShopifyCreatePaymentValid(t *testing.T) {
+	assert := assert.New(t)
+
+	shopifyClient := client.New("key", "secret", "test")
+	svc := payment.New(shopifyClient)
+
+	// test data
+	wd, _ := os.Getwd()
+
+	// test data
+	filePath := path.Join(wd, "testdata", "req_payment_valid.json")
+	b, _ := ioutil.ReadFile(filePath)
+	var p payment.Payment
+	_ = json.Unmarshal([]byte(b), &p)
+
+	// test data
+	filePath = path.Join(wd, "testdata", "res_payment_valid.json")
+	b, _ = ioutil.ReadFile(filePath)
+	var paymentResult map[string]interface{}
+	_ = json.Unmarshal([]byte(b), &paymentResult)
+
+	// prepare our mock
+	r := ioutil.NopCloser(bytes.NewReader([]byte(b)))
+	mockHTTPClient := &mock.HTTPClient{}
+	mockHTTPClient.DoFn = func(*http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 202,
+			Body:       r,
+		}, nil
+	}
+	svc.Shopify.Client.HTTPClient = mockHTTPClient
+
+	result, err := svc.CreatePayment("somevalidtoken", &p)
+	assert.NotNil(result)
+	assert.Nil(err)
+}
